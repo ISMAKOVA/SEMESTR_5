361b3a64f2a1bbb1cd60d31ad1b7321079ffd677
ISMAKOVA <48486359+ISMAKOVA@users.noreply.github.com>
2020-10-28 12:52:46 +0500

    Налажена работа сервис-клиент

---
+++ БИПИТ_2/BIPIT_5/BIPIT_5_CLIENT/BIPIT_5_CLIENT/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace BIPIT_5_CLIENT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var service = new ServiceReference1.ServiceClient();
+            dataGridView1.DataSource = service.GetData().Tables["Moving"];
+           // comboBox1.DataSource = service.GetDataExhibits().Tables["Exhibits"];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ БИПИТ_2/BIPIT_5/BIPIT_5_CLIENT/BIPIT_5_HOST/Service.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BIPIT_5_HOST
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service : IService
+    {
+        string path = "workstation id=gallery.mssql.somee.com;packet size=4096;user id=ismakova_SQLLogin_1;pwd=prad8etyk3;data source=gallery.mssql.somee.com;persist security info=False;initial catalog=gallery";
+
+        public void NewRec(string exh_fk, string halls_fk, string date_start, string date_end)
+        {
+            SqlConnection con = new SqlConnection(path);
+            con.Open();
+            SqlCommand command = new SqlCommand($"insert into Moving (exhibit_fk, halls_fk, date_start,date_end) values(@exh_fk, @halls_fk, @date_start, @date_end)", con);
+            command.Parameters.AddWithValue("@exh_fk", exh_fk);
+            command.Parameters.AddWithValue("@halls_fk", halls_fk);
+            command.Parameters.AddWithValue("@date_start", Convert.ToDateTime(date_start));
+            command.Parameters.AddWithValue("@date_end", Convert.ToDateTime(date_end));
+            command.ExecuteNonQuery();
+            Program.Print(String.Format("[{0}] - Выполнено: запись добавлена",DateTime.Now.ToShortDateString()));
+            con.Close();
+        }
+
+        public DataSet GetData()
+        {
+            string query = "select Moving.id as 'ID', Exhibits.exhibit as 'Экспонат', Exhibits.name as 'Название', Exhibits.author as 'Автор', Halls.museum as 'Музей',  format(Moving.date_start,'dd.MM.yyyy г.') as 'Дата поступления работы',format(Moving.date_end,'dd.MM.yyyy г.') as 'Дата отъезда работы' from Exhibits, Halls, Moving where Moving.exhibit_fk = Exhibits.id_exhibit and Moving.halls_fk = Halls.id_hall";
+            return SqlSelectData("Moving", query);
+        }
+
+        public DataSet GetDataExhibits()
+        {
+            return SqlSelectData("Exhibits", "select * from Exhibits");
+        }
+
+        public DataSet GetDataHalls()
+        {
+            return SqlSelectData("Halls", "select * from Halls");
+        }
+
+        private DataSet SqlSelectData(string tableName, string query)
+        {
+            DataSet dataSet = new DataSet();
+            SqlConnection con = new SqlConnection(path);
+            SqlCommand command = new SqlCommand(query, con);
+
+            con.Open();
+            SqlDataAdapter sqlData1 = new SqlDataAdapter(command);
+            sqlData1.Fill(dataSet, tableName);
+            con.Close();
+            Program.Print(String.Format("[{0}] - Выполнено: данные получены | {1}", DateTime.Now.ToShortDateString(),tableName));
+            return dataSet;
+        }
+    }
+}
