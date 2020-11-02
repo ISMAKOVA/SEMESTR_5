3ec098d4a948cf98e57423c3fdf1fbbd83a4c3bc
ISMAKOVA <48486359+ISMAKOVA@users.noreply.github.com>
2020-11-02 16:52:22 +0500

    Создана веб форма + подключен червис

---
+++ БИПИТ_2/BIPIT_6/BIPIT_5_WCF/BIPIT_5_HOST/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace BIPIT_5_HOST
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        DataSet GetData();
+
+        [OperationContract]
+        void NewRec(string exh_fk, string halls_fk, string date_start, string date_end);
+
+        [OperationContract]
+        DataSet GetDataExhibits();
+
+        [OperationContract]
+        DataSet GetDataHalls();
+    }
+}
---
+++ БИПИТ_2/BIPIT_6/BIPIT_5_WCF/BIPIT_6_WEB_CLIENT/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace BIPIT_6_WEB_CLIENT
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var service = new ServiceReference1.ServiceClient();
+            var exhibits = service.GetDataExhibits().Tables["Exhibits"];
+            var halls = service.GetDataHalls().Tables["Halls"];
+            Table1.DataSource = service.GetData().Tables["Moving"];
+            Table1.DataBind();
+            if (!IsPostBack)
+            {
+                FillDropList(exhibits, exhibit_list);
+                FillDropList(halls, halls_list);
+            }
+        }
+        protected void Add(object sender, EventArgs e)
+        {
+            var c_from = from.Text;
+            var c_to = to.Text;
+            if (c_from != "" || c_to != "")
+            {
+                if (Convert.ToDateTime(c_from) > Convert.ToDateTime(c_to))
+                {
+                    error.Text = "Вторая дата не должна быть меньше первой";
+                    return;
+                }
+                var service = new ServiceReference1.ServiceClient();
+                var exhibit_id = exhibit_list.Text.Split(new char[] { '-' })[0];
+                var hall_id = halls_list.Text.Split(new char[] { '-' })[0];
+
+                service.NewRec(exhibit_id, hall_id, c_from, c_to);
+                Page_Load(sender, e);
+            }
+            else
+            {
+                error.Text = "Выберите даты";
+                return;
+            }
+        }
+        protected void FillDropList(DataTable dataTable, DropDownList list)
+        {
+            list.Items.Clear();
+            DataTable data = dataTable;
+
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                list.Items.Add(string.Join("-", data.Rows[i].ItemArray));
+            }
+            list.DataBind();
+        }
+    }
+}
